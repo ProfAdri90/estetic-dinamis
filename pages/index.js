a02023be7a5a725a6d4e7049ea35e69320169665
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-21 20:07:28 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+/*import { useState } from "react";*/
 /*import { motion } from "framer-motion";*/
 import { motion, AnimatePresence } from "framer-motion";
 
