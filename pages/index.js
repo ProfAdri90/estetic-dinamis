8afa958a7326aaa9d2a59847f74b8143a90b469d
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-23 10:08:58 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -280,23 +280,7 @@ useEffect(() => {
   ))}
 
   {/* Layering Gelap */}
-  <div className="absolute inset-0 bg-black/45 z-10"></div>
-
-  {/* Konten Utama - Logo & Teks */}
-  <div className="relative z-20 flex flex-col items-center">
-    <img
-      src="/logo.png"
-      alt="Estetic Logo"
-      className="h-36 md:h-44 mb-2 drop-shadow-xl"
-    />
-    <p className="max-w-xl text-lg drop-shadow text-[#d7b940] font-medium">
-      {lang === "id"
-        ? "Kami memahami dinamika industri modern, dan kami siap untuk memastikan pesan Anda sampai dengan tepat, berdampak dan bernilai."
-        : "We understand the dynamics of modern industries and are ready to ensure your message is delivered accurately, impactfully, and meaningfully."}
-    </p>
-  </div>
-</section>
-
+  <div className="absolute inset-0 bg-black/60 z-10"></div>
 
   {/* Overlay agar teks lebih terbaca */}
  <div className="flex flex-col items-center justify-center gap-2 mt-4">
@@ -307,12 +291,12 @@ useEffect(() => {
       className="h-full w-auto object-contain mx-auto"
     />
   </div>
-   <p className="max-w-xl text-lg drop-shadow text-[#082846] font-medium">
+  <p className="max-w-xl text-lg drop-shadow">
       {lang === "id"
         ? "Kami memahami dinamika industri modern, dan kami siap untuk memastikan pesan Anda sampai dengan tepat, berdampak dan bernilai."
         : "We understand the dynamics of modern industries and are ready to ensure your message is delivered accurately, impactfully, and meaningfully."}
     </p>
-  </div>
+</div>
 
 </section>
 
