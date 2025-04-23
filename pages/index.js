3b66a2ca90d660bf8c82a289abbeb9d2a6189528
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-23 21:20:41 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import PartnerMedia from "@/components/PartnerMedia";
+import PartnerMedia from "../components/PartnerMedia";
 /*import { useState } from "react";*/
 /*import { motion } from "framer-motion";*/
 import { motion, AnimatePresence } from "framer-motion";
