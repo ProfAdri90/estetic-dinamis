dd2e4f9bb7479f3b8818121996d4cea2ffde59c8
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-25 14:45:18 +0700

    Update TeamCard.jsx

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -11,12 +11,22 @@ export default function TeamCard({ name, title, image }) {
     height={200}
     className="object-cover w-full h-[180px] filter grayscale group-hover:grayscale-0 transition-all duration-500 ease-in-out"
   />
-  <div className="absolute bottom-0 left-0 w-full backdrop-blur-md bg-black/50 p-2 opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0 transition-all duration-500 ease-in-out">
-    <h3 className="text-white text-sm font-semibold">{name}</h3>
-    <hr className="my-1 border-t border-gray-300 w-8" />
-    <p className="text-xs text-gray-200">{title}</p>
+ <div className="w-[12.5%] px-1">
+  <div className="relative group w-full aspect-[3/4] overflow-hidden rounded-xl shadow-md">
+    <Image
+      src={image}
+      alt={name}
+      fill
+      className="object-cover filter grayscale group-hover:grayscale-0 transition-all duration-500 ease-in-out"
+    />
+    <div className="absolute bottom-0 left-0 w-full backdrop-blur-md bg-black/50 p-2 opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0 transition-all duration-500 ease-in-out">
+      <h3 className="text-white text-sm font-semibold">{name}</h3>
+      <hr className="my-1 border-t border-gray-300 w-8" />
+      <p className="text-xs text-gray-200">{title}</p>
+    </div>
   </div>
 </div>
+
   );
 }
 
