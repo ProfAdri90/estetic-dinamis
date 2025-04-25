e34d03f66d5b37c1bc47ccfe695c6b9017efc89b
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-25 22:30:50 +0700

    Update TeamCard.jsx

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function TeamCard({ image, name, title }) {
   return (
-    <div className="w-[240px] aspect-[2/3] relative group overflow-hidden rounded-md shadow-md"> 
+    <div className="w-[280px] aspect-[2/3] relative group overflow-hidden shadow-md">
       <Image
         src={image}
         alt={name}
@@ -12,13 +12,17 @@ export default function TeamCard({ image, name, title }) {
       />
 
       <div className={`
-        absolute bottom-0 left-0 w-full px-3 py-2 backdrop-blur-sm bg-black/60
-        opacity-100 md:opacity-0 md:group-hover:opacity-100
-        translate-y-0 md:translate-y-full md:group-hover:translate-y-0
+        absolute bottom-0 left-0 w-full px-3 py-2
+        backdrop-blur-sm bg-black/60
         transition-all duration-500 ease-in-out
+        text-white text-center
+
+        md:opacity-0 md:group-hover:opacity-100
+        md:translate-y-full md:group-hover:translate-y-0
+        opacity-100 translate-y-0
       `}>
-        <h3 className="text-white text-sm font-semibold">{name}</h3>
-        <hr className="my-1 border-t border-gray-300 w-6" />
+        <h3 className="text-sm font-semibold">{name}</h3>
+        <hr className="my-1 border-t border-gray-300 w-6 mx-auto" />
         <p className="text-xs text-gray-200">{title}</p>
       </div>
     </div>
