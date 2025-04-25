62d31ca6fe06fbc7cea570b5d477d7189cb19c7f
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-25 11:38:54 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -567,6 +567,11 @@ useEffect(() => {
   </div>
 </section>
 <section id="sukses" className="relative w-full h-screen overflow-hidden">
+    <h2 className="text-4xl md:text-5xl font-bold text-center text-[#d7b940] drop-shadow-lg mb-8 z-20 relative">
+    {lang === 'id'
+      ? 'Perjalanan Keberhasilan Estetic dari Waktu ke Waktu'
+      : 'Estetic’s Legacy of Excellence Through the Years'}
+  </h2>
   {/* Background Image Crossfade */}
   <AnimatePresence mode="wait">
     <motion.div
