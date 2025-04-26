8ca1af18e53a404dd41992b4e437b605f1f2d253
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-26 08:11:37 +0700

    Update OurTeams.jsx

--- components/OurTeams.jsx
+++ components/OurTeams.jsx
@@ -28,7 +28,7 @@ export default function OurTeams() {
       </motion.h2>
 
       {/* Grid Team */}
-      <div className="grid grid-cols-2 md:grid-cols-8 gap-4 w-full">
+      <div className="grid grid-cols-2 md:grid-cols-8 gap-4 w-full px-4">
         {teamMembers.map((member, index) => (
           <motion.div
             key={index}
