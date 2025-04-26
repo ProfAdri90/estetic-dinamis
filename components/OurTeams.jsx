4b188b8ce4c8a51bda715d60f4734bbf08891701
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-26 09:08:58 +0700

    Update OurTeams.jsx

--- components/OurTeams.jsx
+++ components/OurTeams.jsx
@@ -22,7 +22,7 @@ export default function OurTeams() {
             key={index}
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: index * 0.05 }}
+            transition={{ duration: 0.5, delay: index * 0.05 }} 
             viewport={{ once: true }}
           >
             <TeamCard
