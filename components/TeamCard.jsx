afe4aae88fe04841d71b2468060dc8e8f145c9c0
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-26 08:10:01 +0700

    Update TeamCard.jsx

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -6,7 +6,7 @@ export default function TeamCard({ image, name, title }) {
     <motion.div
       whileHover={{ scale: 1.05 }}
       transition={{ type: "spring", stiffness: 300, damping: 20 }}
-      className="relative w-full aspect-[3/4] overflow-hidden rounded-xl group shadow-md hover:shadow-2xl transition-all duration-500"
+      className="relative w-full aspect-[3/4] overflow-hidden group shadow-md hover:shadow-xl"
     >
       <Image
         src={image}
@@ -15,14 +15,13 @@ export default function TeamCard({ image, name, title }) {
         className="object-cover transition duration-500 md:grayscale md:group-hover:grayscale-0"
       />
 
-      {/* Overlay Nama + Jabatan */}
       <div
-        className="absolute bottom-0 w-full bg-black/60 backdrop-blur-md text-center py-2
-          md:opacity-0 md:group-hover:opacity-100
-          transition-all duration-500 ease-in-out text-xs md:text-sm"
+        className="absolute bottom-0 w-full bg-black/60 backdrop-blur-md text-center py-2 
+        md:opacity-0 md:group-hover:opacity-100 
+        transition-all duration-500 ease-in-out text-white text-xs md:text-sm"
       >
-        <h3 className="text-xs md:text-sm font-bold">{name}</h3>
-        <p className="text-[10px] md:text-xs">{title}</p>
+        <h3 className="font-bold">{name}</h3>
+        <p>{title}</p>
       </div>
     </motion.div>
   );
