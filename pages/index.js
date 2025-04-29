b449ba9d4ccbd5389202097b2a05cbadd454cb35
ProfAdri90 <adrimuhammad17@gmail.com>
2025-04-29 18:14:44 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -667,7 +667,7 @@ useEffect(() => {
     </div>
   </div>
 </section>
-<section id="our-team" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-4 py-12 scroll-mt-[140px]">
+<section id="tim" className="scroll-mt-[140px] py-12">
   <h3 className="text-3xl font-bold mb-10 text-center">
     {lang === "id" ? "Tim Kami" : "Our Team"}
   </h3>
