76205a57b0582f181490bae17f4f92481f442367
ProfAdri90 <adrimuhammad17@gmail.com>
2025-05-27 09:47:54 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,10 @@
-import '../styles/globals.css'; export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
+import '../styles/globals.css';
+import { AuthProvider } from '@/contexts/AuthContext'; // pastikan path-nya sesuai ya
+
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
