dd1c3245ab58ca280055862d5dc66ab16de9357b
ProfAdri90 <adrimuhammad17@gmail.com>
2025-07-29 16:52:08 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -797,7 +797,7 @@ useEffect(() => {
 </div>
   </div>
 </section>
-<CilentCategory lang={lang} />
+<ClientCategory lang={lang} />
  <section className="bg-white text-[#082846] py-20 px-4" id="kontak">
   <div className="max-w-7xl mx-auto">
     <div className="grid grid-cols-1 md:grid-cols-3 gap-12 items-start">
