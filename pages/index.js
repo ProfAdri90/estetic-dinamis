ba7e0ab1e8f849209ceb4cf911db2dd7a76905d8
ProfAdri90 <adrimuhammad17@gmail.com>
2025-07-29 16:48:37 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import PartnerMedia from "../components/PartnerMedia";
 /*import { motion } from "framer-motion";*/
 import { motion, AnimatePresence } from "framer-motion";
 import OurTeams from "../components/OurTeams";
-import ClientGrid from "../components/ClientGrid";
+import ClientCategory from "@/components/ClientCategory";
 import Testimonials from "../components/Testimonials";
 
 
